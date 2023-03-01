9c2fbf9247fea5a0ffb0eb91347eae7910c615a3
dev-victoerw <dev.victorw@gmail.com>
2023-03-01 18:38:33 -0300

    Implementando Step_definitions de Candidato - Parte 3

--- features/step_definitions/candidato_steps.rb
+++ features/step_definitions/candidato_steps.rb
@@ -26,7 +26,7 @@ Given("que estou na página de listagem de candidatos") do
   visit candidates_path
 end
 
-When("clico no botão {string} do candidato que desejo excluir") do |button_text|
+When("clico no botão Deletar do candidato que desejo excluir") do |button_text|
   candidate = Candidate.create(nome: "João da Silva", email: "joao.silva@example.com", telefone: "(11) 99999-9999")
   within "#candidate_#{candidate.id}" do
     click_on button_text
@@ -43,4 +43,31 @@ end
 
 And("o cadastro deve ser excluído do banco de dados") do
   expect(Candidate.count).to eq(0)
+end
+
+#EDITAR CADASTRO
+Given("que estou na página de edição de um cadastro existente") do
+  candidate = Candidate.create(nome: "João da Silva", email: "joao.silva@example.com", telefone: "(11) 99999-9999")
+  visit edit_candidate_path(candidate)
+end
+
+When("preencho os campos que desejo atualizar com informações válidas") do
+  fill_in "Nome", with: "Maria Souza"
+  fill_in "Email", with: "maria.souza@example.com"
+  fill_in "Telefone", with: "(11) 88888-8888"
+end
+
+And("o cadastro deve ser atualizado no banco de dados") do
+  candidate = Candidate.first
+  expect(candidate.nome).to eq("Maria Souza")
+  expect(candidate.email).to eq("maria.souza@example.com")
+  expect(candidate.telefone).to eq("(11) 88888-8888")
+end
+
+Given("que tento acessar a página de edição de um cadastro que não existe") do
+  visit edit_candidate_path(-1)
+end
+
+Then("devo ver uma mensagem de erro indicando que o cadastro não foi encontrado") do
+  expect(page).to have_content "Cadastro não encontrado."
 end
