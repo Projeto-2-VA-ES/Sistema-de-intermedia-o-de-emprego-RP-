85dc61dfe14384b78214c96e4be1fd3f3c29c82e
LuanValentinoS <luanvalentino@gmail.com>
2023-03-01 22:35:15 -0300

    Implementação do step_definitions de Vaga - Parte 2

--- features/step_definitions/vaga_steps.rb
+++ features/step_definitions/vaga_steps.rb
@@ -31,4 +31,44 @@ Then("eu devo ser redirecionado para a página da vaga selecionada") do
   expect(page).to have_content("Descrição da vaga:")
 end
 
+# Editar Vaga
+Given("que eu esteja na página de edição de vaga") do
+  visit edit_vaga_path(@vaga)
+end
 
+When("eu altero os campos desejados da vaga") do
+  fill_in "Descrição", with: "Vaga para desenvolvedor Ruby on Rails"
+  click_button "Atualizar Vaga"
+end
+
+Then("eu devo ser redirecionado para a página atualizada da vaga") do
+  expect(page).to have_content("Vaga atualizada com sucesso")
+end
+
+
+# Remover Vaga
+Given("que eu esteja na página de listagem de vagas") do
+  visit vagas_path
+end
+
+When("eu clico no botão 'Remover' da vaga desejada") do
+  click_link "Remover"
+end
+
+Then("a vaga deve ser removida da lista de vagas") do
+  expect(page).to_not have_content("Desenvolvedor Ruby")
+end
+
+
+# Ver Lista de Vagas
+Given("que eu esteja na página inicial") do
+  visit root_path
+end
+
+When("eu clico no botão 'Ver Vagas'") do
+  click_link "Ver Vagas"
+end
+
+Then("eu devo ser redirecionado para a página de listagem de vagas") do
+  expect(page).to have_content("Lista de vagas")
+end
