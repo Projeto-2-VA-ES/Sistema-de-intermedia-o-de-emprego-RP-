fb3b30ee98f4291d5837130cf0a7203de74a724d
Luizfdarb <78767627+Luizfdarb@users.noreply.github.com>
2023-03-04 15:03:20 -0300

    Criação testes unitários de empregador

--- test/models/empregador_test.rb
+++ test/models/empregador_test.rb
@@ -1,7 +1,41 @@
 require "test_helper"
 
 class EmpregadorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "salvar empregador com sucesso" do
+    empregador = Empregador.new
+    empregador.nome = " Rian Wilker "
+    empregador.nomeEmpresa = "12345678901"
+    empregador.cpnj = "12.345.678/0001-00"
+    empregador.telefone = "87-99258-9444"
+    empregador.endereco = "Rua Almir pacherco"
+
+    assert empregador.save, "Salvou empregador com sucesso"
+
+  end
+
+  test "salvar empregador sem CNPJ" do
+    empregador = Empregador.new
+    empregador.nome = "Rian Wilker"
+    empregador.nomeEmpresa = "Frios LDTA"
+    empregador.cpnj = "12.345.678/0001-00"
+    empregador.telefone = "87-99258-9444"
+    empregador.endereco = "Rua Almir Pacheco"
+
+    assert empregador.save, "Salvou empregador sem CNPJ"
+
+  end
+
+  test "salvar empregador sem endereço" do
+    empregador = Empregador.new
+    empregador.nome = "Rian Wilker"
+    empregador.nomeEmpresa = "Frios LDTA"
+    empregador.cpnj = "12.345.678/0001-00"
+    empregador.telefone = "87-99258-9444"
+    empregador.endereco = ""
+
+    assert empregador.save, "Salvou empregador sem endereco"
+
+  end
+
 end
