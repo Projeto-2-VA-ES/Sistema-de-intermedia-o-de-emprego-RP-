d3e06ed2312ba954590f5db5704bb862b3b13b8a
Rian Wilker <rianwilke17@gmil.com>
2023-03-06 21:32:02 -0300

    Alteração das rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :candidatos do
+  resources :candidatos
+  resources :empregadors do
     resources :vaga_de_empregos
   end
-
-  resources :empregadors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
