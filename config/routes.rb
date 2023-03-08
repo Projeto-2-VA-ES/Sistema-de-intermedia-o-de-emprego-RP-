a6077510a297e409421d11c39a838127bd697826
dev-victoerw <dev.victorw@gmail.com>
2023-03-08 15:08:27 -0300

    Correção de rotas e correção do model de empregador

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :empregadors do
     resources :vaga_de_empregos
   end
+  resources :vaga_de_empregos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
