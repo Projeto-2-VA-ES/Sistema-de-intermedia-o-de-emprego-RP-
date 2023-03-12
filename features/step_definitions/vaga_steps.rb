16ea947ed41e7f2fa01085779e08fb80219b1f47
LuanValentinoS <luanvalentino@gmail.com>
2023-03-12 20:55:06 -0300

    Remoção dos acentos e caracteres especiais de vaga_steps

--- features/step_definitions/vaga_steps.rb
+++ features/step_definitions/vaga_steps.rb
@@ -1,57 +1,55 @@
 # Adicionar Vaga
-Given("que eu esteja na página de criação de vaga") do
+Given("que eu esteja na pagina de criacao de vaga") do
   visit new_vaga_path
 end
 
-When("eu preencho os campos obrigatórios com os dados da vaga") do
-  fill_in "Título", with: "Desenvolvedor Ruby"
-  fill_in "Descrição", with: "Vaga para desenvolvedor Ruby"
-  fill_in "Salário", with: "R$ 5.000,00"
+When("eu preencho os campos obrigatorios com os dados da vaga") do
+  fill_in "Titulo", with: "Desenvolvedor Ruby"
+  fill_in "Descricao", with: "Vaga para desenvolvedor Ruby"
+  fill_in "Salario", with: "R$ 5.000,00"
 end
 
 And("eu clico em 'Criar Vaga'") do
   click_button "Criar Vaga"
 end
 
-Then("eu devo ser redirecionado para a página da vaga recém-criada") do
+Then("eu devo ser redirecionado para a pagina da vaga recentemente criada") do
   expect(page).to have_content("Vaga criada com sucesso")
 end
 
-
 # Visualizar Vaga
-Given("que eu esteja na página de listagem de vagas") do
+Given("que eu esteja na pagina de listagem de vagas") do
   visit vagas_path
 end
 
-When("eu clico no botão 'Visualizar' da vaga desejada") do
+When("eu clico no botao 'Visualizar' da vaga desejada") do
   click_link "Visualizar"
 end
 
-Then("eu devo ser redirecionado para a página da vaga selecionada") do
-  expect(page).to have_content("Descrição da vaga:")
+Then("eu devo ser redirecionado para a pagina da vaga selecionada") do
+  expect(page).to have_content("Descricao da vaga:")
 end
 
 # Editar Vaga
-Given("que eu esteja na página de edição de vaga") do
+Given("que eu esteja na pagina de edicao de vaga") do
   visit edit_vaga_path(@vaga)
 end
 
 When("eu altero os campos desejados da vaga") do
-  fill_in "Descrição", with: "Vaga para desenvolvedor Ruby on Rails"
+  fill_in "Descricao", with: "Vaga para desenvolvedor Ruby on Rails"
   click_button "Atualizar Vaga"
 end
 
-Then("eu devo ser redirecionado para a página atualizada da vaga") do
+Then("eu devo ser redirecionado para a pagina atualizada da vaga") do
   expect(page).to have_content("Vaga atualizada com sucesso")
 end
 
-
 # Remover Vaga
-Given("que eu esteja na página de listagem de vagas") do
+Given("que eu esteja na pagina de listagem de vagas") do
   visit vagas_path
 end
 
-When("eu clico no botão 'Remover' da vaga desejada") do
+When("eu clico no botao 'Remover' da vaga desejada") do
   click_link "Remover"
 end
 
@@ -59,16 +57,15 @@ Then("a vaga deve ser removida da lista de vagas") do
   expect(page).to_not have_content("Desenvolvedor Ruby")
 end
 
-
 # Ver Lista de Vagas
-Given("que eu esteja na página inicial") do
+Given("que eu esteja na pagina inicial") do
   visit root_path
 end
 
-When("eu clico no botão 'Ver Vagas'") do
+When("eu clico no botao 'Ver Vagas'") do
   click_link "Ver Vagas"
 end
 
-Then("eu devo ser redirecionado para a página de listagem de vagas") do
+Then("eu devo ser redirecionado para a pagina de listagem de vagas") do
   expect(page).to have_content("Lista de vagas")
 end
