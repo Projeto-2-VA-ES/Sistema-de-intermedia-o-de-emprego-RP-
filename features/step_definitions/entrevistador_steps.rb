5cbfe5462c65e0e4487feffc36ec126071a059a2
dev-victoerw <dev.victorw@gmail.com>
2023-03-21 22:55:44 -0300

    Melhorando teste de Entrevistador

--- features/step_definitions/entrevistador_steps.rb
+++ features/step_definitions/entrevistador_steps.rb
@@ -1,9 +1,9 @@
 
-Given('que existe um empregador chamado {string} com a empresa {string} com o email {string} com o endereco {string} com o telefone {string} e com o cnpj {string}') do |nome, empresa, email, endereco, telefone, cnpj|
+Given('que existe um empregador chamado {string} dono da empresa {string} com o email {string} com o endereco {string} com o telefone {string} e com o cnpj {string}') do |nome, empresa, email, endereco, telefone, cnpj|
   @empregador = Empregador.create!(nome: nome, nomeEmpresa: empresa, email: email, endereco: endereco, telefone: telefone, cnpj: cnpj)
 end
 
-And('criou a seguinte vaga de emprego: Titulo: {string}, Descricao: {string} e Salario: {string}') do |titulo, descricao, salario|
+And('tem disponivel a seguinte vaga de emprego: Titulo: {string}, Descricao: {string} e Salario: {string}') do |titulo, descricao, salario|
   @vaga_de_emprego = VagaDeEmprego.create!(titulo: titulo, descricao: descricao, salario: salario, empregador: @empregador)
 end
 
@@ -13,7 +13,7 @@ end
 
 And('preencho os campos para criacao de entrevistador') do
   Entrevistador.create!(nome: "Victor", email: 'victor@gmail.com', telefone: '11-12345-1234')
-  vaga = VagaDeEmprego.find_by(titulo: "Dev junior ruby on rails")
+  vaga = VagaDeEmprego.find_by(titulo: "Dev Pleno ruby on rails")
   select vaga.titulo, from: "entrevistador[vaga_de_emprego_id]"
 end
 
